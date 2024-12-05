827e572319ede8dc924855d65c3c1a5b290b273b
fiqriachmada <achmada1998@gmail.com>
2024-12-05 17:13:02 +0700

    "Updated HTML template for contact form submission email"

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -18,17 +18,29 @@ export async function submitContact(formData: FormData) {
       data: validatedData,
     });
 
-    await resend.emails.send({
+    const toAdmin = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "taufaniafrinda21@gmail.com",
       subject: "New Contact Form Submission",
       html: `
-        <h1>New Contact Form Submission</h1>
-        <p><strong>Name:</strong> ${validatedData.name}</p>
-        <p><strong>Email:</strong> ${validatedData.email}</p>
-        <p><strong>Message:</strong> ${validatedData.message}</p>
-        `,
+        <div class="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md space-y-4">
+  <h1 class="text-2xl font-bold text-gray-800">New Contact Form Submission</h1>
+  <p class="text-gray-600">
+    <strong class="font-semibold text-gray-800">Name:</strong> 
+    <span>${validatedData.name}</span>
+  </p>
+  <p class="text-gray-600">
+    <strong class="font-semibold text-gray-800">Email:</strong> 
+    <span>${validatedData.email}</span>
+  </p>
+  <p class="text-gray-600">
+    <strong class="font-semibold text-gray-800">Message:</strong> 
+    <span>${validatedData.message}</span>
+  </p>
+</div>`,
     });
+
+    console.log('toAdmin', toAdmin)
 
     return { success: true };
   } catch (error) {
